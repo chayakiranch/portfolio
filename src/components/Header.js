560f641ad510012c1c46fa631192d74770bb2ad7
chayakiranch <chayakiranch@gmail.com>
2024-11-12 16:12:48 +0530

    commiting updated header files & App.js & adding images

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import './Header.css'; // Import CSS if you want to style it separately
 
-// Example placeholder for logo; you can replace it with an actual icon or logo.
 const Header = () => {
   return (
     <header className="header">
       <div className="logo">
-        {/* Replace with an actual image or SVG for your logo */}
-        <img src="/images/Your_logo.png" alt="Logo" className="logo-icon" />
+        {/* Ensure the src path is correct for your project setup */}
+        <img src="/images/Your_logo.png" alt="System Design Logo" className="logo-icon" />
       </div>
-      <div className="title">System Design Newsletter</div>
+      <h1 className="title">System Design Newsletter</h1>
       <div className="actions">
-        <button className="subscribe-button">Subscribe</button>
-        <a href="/signin" className="signin-link">Sign in</a>
+        <button className="subscribe-button" aria-label="Subscribe to the newsletter">Subscribe</button>
+        <a href="/signin" className="signin-link" aria-label="Sign in to your account">Sign in</a>
       </div>
     </header>
   );
